e4cf187c6f47a8bcae43a7566ac482e996b54531
AWBog <agenciawebbogota@gmail.com>
2018-07-30 11:02:09 -0500

    Agregando inicio de sesión con google

---
+++ public/js/login.js
@@ -0,0 +1,37 @@
+new Vue({
+  el:'#app',
+  data:{
+
+  },
+  methods:{
+    google(){
+
+        var provider = new firebase.auth.GoogleAuthProvider();
+        // [END createprovider]
+        // [START addscopes]
+        provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+        firebase.auth().languageCode = 'pt';
+        // provider.setCustomParameters({
+        //   'login_hint': 'user@example.com'
+        // });
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // The signed-in user info.
+          var user = result.user;
+          // ...
+        }).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          console.log(errorMessage);
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+        });
+
+    }
+  }
+})
